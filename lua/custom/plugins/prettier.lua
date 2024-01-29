0ec4b98596ec8e95d2ff1457133e259163063eed
Dominik Lutley <domlutley@gmail.com>
2024-01-29 22:12:35 +0100

    prettier

---
+++ lua/custom/plugins/prettier.lua
@@ -0,0 +1,12 @@
+return {
+	"MunifTanjim/prettier.nvim",
+	dependencies = { "jose-elias-alvarez/null-ls.nvim" },
+	opts = {
+		bin = "prettier",
+		filetypes = {
+			"css",
+			"html",
+			"htmldjango",
+		},
+	},
+}
